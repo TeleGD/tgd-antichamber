3a7283cc1191847b14d26bc004097650e6296c79
stephane-hulot <hulot.stephane.55@gmail.com>
2019-11-21 16:26:11 +0100

    Controleur de joueur et téléportation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 10;
+
+    private Vector2 viewAngle;
+    private Rigidbody body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        dir = dir.sqrMagnitude > 1 ? dir.normalized : dir;
+        dir *= walkSpeed;
+        dir = transform.rotation * dir;
+        body.velocity = dir;
+    }
+
+    void Update()
+    {
+        //entrée souris
+        viewAngle.x += Input.GetAxis("Mouse X");
+        viewAngle.y = Mathf.Clamp(viewAngle.y - Input.GetAxis("Mouse Y"), -80, 80);
+
+        //rotation de la vue
+        transform.GetChild(0).localEulerAngles = new Vector3(viewAngle.y, 0, 0);
+        transform.eulerAngles = new Vector3(0, viewAngle.x, 0);
+    }
+
+    public void RotatePlayer(float amount)
+    {
+        viewAngle.x += amount;
+    }
+}
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public Vector3 offset;
+    public float angle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.transform.position = RotatePointAroundPivot(other.transform.position, transform.position, Vector3.down * angle);
+            other.transform.position += offset;
+            other.GetComponent<PlayerController>().RotatePlayer(angle);
+        }
+    }
+
+    public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
+    {
+        return Quaternion.Euler(angles) * (point - pivot) + pivot;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Collider trigger = GetComponent<Collider>();
+        Gizmos.color = new Color(0, 0.5f, 1, 0.2f);
+        Gizmos.DrawCube(trigger.bounds.center, trigger.bounds.size);
+        Gizmos.color = new Color(1, 0.8f, 0, 0.2f);
+        Gizmos.DrawCube(trigger.bounds.center + offset, trigger.bounds.size);
+    }
+}
